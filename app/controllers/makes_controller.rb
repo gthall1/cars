ff422bdc3368bbfcd91ff3d24ba7a3948ea7ee4f
Griff Hall <griffhall1@gmail.com>
2014-07-02 23:54:20 -0400

    added simple form gem, created new items in makes controller, added flash errors to makes controller

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -3,4 +3,17 @@ class MakesController < ApplicationController
     @make = Make.all
   end
 
+  def new
+    @new_make = Make.new
+  end
+
+  def create
+    @make = Make.new(make_params)
+    if @make.save
+      redirect_to makes_path
+    else
+      flash[:notice] ="Please fill in all fields completely"
+    end
+  end
+
 end
